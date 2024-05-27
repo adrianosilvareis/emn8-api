597eff3836661b45aa2c0171db9419c8d517c50c
Adriano Reis <adriano_silvareis@hotmail.com>
2024-05-27 15:01:08 -0300

    chore: configure jest and added express

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
