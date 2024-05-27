9ab7ad34179842ee7146ceb63ec079abccc96b69
Adriano Reis <adriano_silvareis@hotmail.com>
2024-05-27 19:47:32 -0300

    feat: created update configuration in express

--- src/employees/main/employee.build.ts
+++ src/employees/main/employee.build.ts
@@ -1,9 +1,11 @@
 import { DeleteEmployeeController } from "../infrastructure/controllers/delete-employee.controller";
 import { GetEmployeeByIdController } from "../infrastructure/controllers/get-employee-by-id.controller";
+import { UpdateEmployeeController } from "../infrastructure/controllers/update-employee.controller";
 import { PostgresEmployeeDatabase } from "../infrastructure/gateways/database/postgres-employee.database";
 import { DeleteEmployeeRepository } from "../infrastructure/repositories/delete-employee.repository";
 import { GetAllEmployeesRepository } from "../infrastructure/repositories/get-all-employees.repository";
 import { GetEmployeeByIdRepository } from "../infrastructure/repositories/get-employee-by-id.repository";
+import { UpdateEmployeeRepository } from "../infrastructure/repositories/update-employee.repository";
 import { GetAllEmployeesController } from "./../infrastructure/controllers/get-all-employees.controller";
 
 const database = new PostgresEmployeeDatabase();
@@ -21,4 +23,9 @@ export const getEmployeeByIdController = new GetEmployeeByIdController(
 const deleteEmployeeById = new DeleteEmployeeRepository(database);
 export const deleteEmployeeController = new DeleteEmployeeController(
   deleteEmployeeById
+);
+
+const updateEmployee = new UpdateEmployeeRepository(database);
+export const updateEmployeeController = new UpdateEmployeeController(
+  updateEmployee
 );
--- src/employees/main/index.ts
+++ src/employees/main/index.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import {
   deleteEmployeeController,
   getAllEmployeesController,
-  getEmployeeByIdController
+  getEmployeeByIdController,
+  updateEmployeeController
 } from "./employee.build";
 
 export const employeeRouters = (app: Router) => {
   app.get("/employee/get-all", routerAdapter(getAllEmployeesController));
   app.get("/employee/:id", routerAdapter(getEmployeeByIdController));
   app.delete("/employee/:id", routerAdapter(deleteEmployeeController));
+  app.put("/employee/:id", routerAdapter(updateEmployeeController));
   return app;
 };
